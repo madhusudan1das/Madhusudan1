afa4d5f93dc3c52b92b1f8a77e3d289a97268304
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2026-02-04 00:21:39 +0530

    Fix cookie SameSite policy for cross-domain usage

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -15,8 +15,8 @@ export const generateToken = (userId, res) => {
   const cookieOptions = {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: ENV.NODE_ENV === "development" ? "lax" : "strict",
-    secure: ENV.NODE_ENV === "development" ? false : true,
+    sameSite: ENV.NODE_ENV === "development" ? "lax" : "none",
+    secure: ENV.NODE_ENV !== "development",
   };
   console.log("Cookie Options:", cookieOptions);
 
